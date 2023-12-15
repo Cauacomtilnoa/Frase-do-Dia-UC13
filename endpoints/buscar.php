1f29bf0f57a1ac52e64129b482a9e27f32ef35d6
Cauacomtilnoa <126720628+Cauacomtilnoa@users.noreply.github.com>
2023-12-15 10:18:53 -0300

    lllll

--- endpoints/buscar.php
+++ endpoints/buscar.php
@@ -1,5 +1,24 @@
 <?php
-
+// Importar as dependências:
+    require_once("../classes/Frase.class.php");
+// Especificar que é um arquivo json:
+    header('Content-Type: application/json');
+    
 // Receber o nome do filme por get e retornar o resultado em json:
 
+// Verificar se o "q" está setado
+    if(isset($_GET["q"])) {
+        // Fazer a busca:
+        $f = new Frase();
+        $f->filme = $_GET['q'];
+
+        // Buscar no BD:
+        $r = $f->BuscarPorNomeFilme();
+        echo json_encode($r);
+    } else {
+        // Retornar um erro:
+
+        $arr = [];
+        echo json_encode($arr);
+    }
 ?>
--- endpoints/listar.php
+++ endpoints/listar.php
@@ -1,6 +1,22 @@
 <?php
+// Especificar que é um arquivo json:
+   header('Content-Type: application/json');
+// Importar as dependências:
+ require_once("../classes/Frase.class.php");
+
+
+ // Instanciar um objeto do tipo frase:
+ $f = new Frase();
 
 // Verificar se aleatorio=1 ou listar todas:
 
-
+    if(isset($_GET["aleatorio"]) && $_GET["aleatorio"] == 1){
+        // Listar aleatório
+        $r = $f->ListarAleatoria();
+        echo json_encode($r);
+    } else {
+        // Listar tudo
+        $r = $f->ListarTodas();
+        echo json_encode($r);
+    }
 ?>
